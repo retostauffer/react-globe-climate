ca4dd6823201d03cf907ad420e3b749d35fae1af
Reto Stauffer <reto.stauffer@uibk.ac.at>
2022-04-19 10:15:56 +0200

    feat: Adding overlay for details

---
+++ frontend/details.php
@@ -0,0 +1,21 @@
+<?php
+
+// GET args handed over
+$args = $_POST;
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+</head>
+<style>
+body {
+background-color: black;
+}
+<body>
+asdf
+</body>
+
+</html>
+
+
